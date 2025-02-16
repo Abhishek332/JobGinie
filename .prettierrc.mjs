c4c1e496e5158641d61a7cf884f760496ce4419f
abhishek332 <itsabhiofficial.21@gmail.com>
2025-02-16 15:39:58 +0530

    add prettier configs

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+    semi: true,
+    singleQuote: true,
+    singleAttributePerLine: true,
+    printWidth: 80,
+    tabWidth: 2,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+    plugins: ['prettier-plugin-tailwindcss'],
+    proseWrap: 'always',
+  };
+
+export default config;
+  
