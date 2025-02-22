e3078bd9e0c314ecf929d052bd7596a3bf5269f0
abhishek332 <itsabhiofficial.21@gmail.com>
2025-02-22 17:55:33 +0530

    fix:make header in center

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -7,7 +7,7 @@ interface SectionTitleProps {
 
 const SectionHeader = ({ title, desc }: SectionTitleProps) => {
   return (
-    <div className="mb-12 w-full max-w-3xl text-center">
+    <div className="mx-auto mb-12 max-w-3xl text-center">
       <h2 className="mb-4 text-3xl font-bold tracking-tighter">{title}</h2>
       {desc && <p className="text-muted-foreground">{desc}</p>}
     </div>
