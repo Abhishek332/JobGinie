caf1294e0e93975db1fdf1aae9aef6a0a7974303
abhishek332 <itsabhiofficial.21@gmail.com>
2025-03-09 15:27:25 +0530

    fix: a eslint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CallToActions from '@/components/cta';
 import FAQs from '@/components/faqs';
 import Features from '@/components/features';
 import HeroSection from '@/components/hero-section';
@@ -5,7 +6,6 @@ import './globals.css';
 import HowItWorks from '@/components/how-it-works';
 import StatsSection from '@/components/stats-section';
 import Testimonials from '@/components/testimonials';
-import CallToActions from '@/components/cta';
 
 export default function Home() {
   return (
