dff6459c5b360736fd262e296bf20be82728a19b
abhishek332 <itsabhiofficial.21@gmail.com>
2025-03-18 14:49:56 +0530

    fix: create server actions for update industry

---
+++ actions/user.ts
@@ -0,0 +1,93 @@
+'use server';
+
+import { auth } from '@clerk/nextjs/server';
+
+import { db } from '@/lib/prisma';
+
+const checkUserAuth = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error('Unauthorized.');
+  }
+
+  const dbUser = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+
+  if (!dbUser) throw new Error('User not found');
+
+  return dbUser;
+};
+
+export async function updateUser(data: unknown) {
+  const dbUser = await checkUserAuth();
+
+  try {
+    const res = await db.$transaction(
+      async (tx) => {
+        console.log('abhishek', tx);
+        const industryInsights = await tx.industryInsight.findUnique({
+          where: {
+            industry: data.industry,
+          },
+        });
+
+        if (!industryInsights) {
+          await tx.industryInsight.create({
+            data: {
+              industry: data.industry,
+              growthRate: 0,
+              demandLevel: 'Medium',
+              marketOutlook: 'Neutral',
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            },
+          });
+        }
+
+        const updatedUser = await tx.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills,
+          },
+        });
+
+        return { updatedUser, industryInsights };
+      },
+      {
+        timeout: 10000,
+      },
+    );
+
+    return res.updatedUser;
+  } catch {
+    throw new Error('Failed to update use profile.');
+  }
+  return dbUser;
+}
+
+export async function getUserOnboardingStatus() {
+  const dbUser = await checkUserAuth();
+
+  try {
+    const userWithIndustry = await db.user.findUnique({
+      where: {
+        id: dbUser.id,
+      },
+      select: {
+        industry: true,
+      },
+    });
+
+    return { isOnboarded: !!userWithIndustry?.industry };
+  } catch {
+    throw new Error('Failed to get user onboarding status.');
+  }
+}
