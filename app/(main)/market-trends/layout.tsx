a20aefd5d5cb14014e0c9e03994997fb64d41200
abhishek332 <itsabhiofficial.21@gmail.com>
2025-04-19 23:18:48 +0530

    create layout for market trends page

---
+++ app/(main)/market-trends/layout.tsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react';
+import { RingLoader } from 'react-spinners';
+
+const PagesLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container mx-auto mb-20 mt-24 px-5">
+      <div className="mb-5 flex items-center justify-between">
+        <h1 className="gradient-title text-6xl font-bold">Market Trends</h1>
+      </div>
+      <Suspense
+        fallback={
+          <RingLoader
+            className="mt-4"
+            color="gray"
+            size={50}
+          />
+        }
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+};
+
+export default PagesLayout;
