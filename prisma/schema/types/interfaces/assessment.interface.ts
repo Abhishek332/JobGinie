49884d4e2a2748329566ddd21929db120ba0853f
abhishek332 <itsabhiofficial.21@gmail.com>
2025-04-20 16:54:47 +0530

    Revert "create inferred types from prisma schemas"
    This reverts commit e80e3d7b5531bb981123bd9f21891a2b745f70ac.

--- prisma/schema/types/interfaces/assessment.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const assessment = Prisma.validator<Prisma.AssessmentDefaultArgs>()({});
-export type Assessment = Prisma.AssessmentGetPayload<typeof assessment>;
--- prisma/schema/types/interfaces/coverletter.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const coverLetter = Prisma.validator<Prisma.CoverLetterDefaultArgs>()({});
-export type CoverLetter = Prisma.CoverLetterGetPayload<typeof coverLetter>;
