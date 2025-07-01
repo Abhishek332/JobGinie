e62f1c53b871a1b10c0802efced0f37215611681
abhishek332 <itsabhiofficial.21@gmail.com>
2025-07-01 23:03:53 +0530

    fix: cleanup some code

--- actions/industry-trends.ts
+++ actions/industry-trends.ts
@@ -4,6 +4,7 @@ import { GoogleGenAI } from '@google/genai';
 import { isEmpty } from 'lodash';
 
 import { checkUserAuth } from './validate-user-auth';
+import { IndustryInsightSchemaForLLM } from '@/llm-schemas/industryInsights.schema';
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
@@ -12,37 +13,19 @@ export async function getIndustryTrends(industry: string) {
 
   if (isEmpty(dbUser.industryInsight)) {
     try {
-      const prompt = `
-          Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
-          {
-            "salaryRanges": [
-              { "role": "string", "min": number, "max": number, "median": number, "location": "string" }
-            ],
-            "growthRate": number,
-            "demandLevel": "High" | "Medium" | "Low",
-            "topSkills": ["skill1", "skill2"],
-            "marketOutlook": "Positive" | "Neutral" | "Negative",
-            "keyTrends": ["trend1", "trend2"],
-            "recommendedSkills": ["skill1", "skill2"]
-          }
-          
-          IMPORTANT: Return ONLY the JSON. No additional text, notes, or markdown formatting.
-          Include at least 5 common roles for salary ranges.
-          Growth rate should be a percentage.
-          Include at least 5 skills and trends.
-        `;
-
       const response = await ai.models.generateContent({
         model: 'gemini-1.5-flash',
-        contents: prompt,
-        // tokenLimit: 4096,
+        contents: `Analyze the current state of the ${industry} industry and provide insights`,
+        config: {
+          responseMimeType: 'application/json',
+          responseSchema: IndustryInsightSchemaForLLM,
+        },
       });
 
-      const cleanedupResponse = (response.text ?? '{}').replaceAll(
-        /```|json|\n/g,
-        '',
+      const jsonResponse = JSON.parse(
+        response.candidates?.[0]?.content?.parts?.[0]?.text ?? '{}',
       );
-      const jsonResponse = JSON.parse(cleanedupResponse);
+
       return jsonResponse;
     } catch (error) {
       console.error('Error generating industry insights:', error);
